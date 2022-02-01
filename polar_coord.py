3c8be1f3e2be4c0e49486ef4413cfa263ce6336d
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2022-02-01 15:05:55 -0800

    Added more arguments to the function, to make it more specific.

--- polar_coord.py
+++ polar_coord.py
@@ -9,7 +9,7 @@ def scale(value, v_min, v_max, r_min, r_max):
     percentage = (value - v_min) / (v_max - v_min)
     return round(r_min + percentage * (r_max - r_min), 2)
 
-def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, scale_vals: tuple):
+def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, x_orig: tuple, y_orig: tuple, x_scaled: tuple, y_scaled: tuple):
 
     pyautogui.moveTo(origin[0], origin[1])
 
@@ -19,8 +19,8 @@ def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, s
         x = r * math.cos(theta)
         y = r * math.sin(theta)
 
-        x = scale(x, -8, 8, scale_vals[0], scale_vals[1])
-        y = scale(y, -8, 8, scale_vals[0], scale_vals[1])
+        x = scale(x, x_orig[0], x_orig[1], x_scaled[0], x_scaled[1])
+        y = scale(y, y_orig[0], y_orig[1], y_scaled[0], y_scaled[1])
 
         x += origin[0]
         y += origin[1]
@@ -30,11 +30,13 @@ def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, s
         if count == 0:
             pyautogui.moveTo(x, y)
         else:
-            pyautogui.dragTo(x, y, duration=.025)
+            pyautogui.dragTo(x, y, duration=.05)
         count += 1
 
 screen_size = pyautogui.size()
 
 print(screen_size)
 
-draw((screen_size.width / 2, screen_size.height / 2 - 50), (7, 7, 10), 1, math.pi / 192, (-200, 200))
+draw((screen_size.width / 2 + 100, screen_size.height / 2 + 75), (7, 20, 1), 1, math.pi / 192, (-.62, 27), (-15.2, 15.2), (-300, 300), (-300, 300))
+draw((screen_size.width / 2 - 100, screen_size.height / 2 + 75), (7, -20, 1), 1, math.pi / 192, (-27, .62), (-15.2, 15.2), (-300, 300), (-300, 300))
+
