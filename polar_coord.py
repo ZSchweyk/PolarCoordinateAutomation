f15cd79b52904d444a514b94c42a0866bef67224
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2022-02-02 09:29:00 -0800

    created an equation parameter, and the eval function helps execute that string.

--- polar_coord.py
+++ polar_coord.py
@@ -3,20 +3,21 @@ import time
 import math
 import numpy as np
 
-time.sleep(10)
+time.sleep(3)
+
 
 def scale(value, v_min, v_max, r_min, r_max):
     percentage = (value - v_min) / (v_max - v_min)
     return round(r_min + percentage * (r_max - r_min), 2)
 
-def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, x_orig: tuple, y_orig: tuple, x_scaled: tuple, y_scaled: tuple):
 
+def draw(origin: tuple, equation: str, num_rot: float, increment: float, x_orig: tuple, y_orig: tuple,
+         x_scaled: tuple, y_scaled: tuple):
     pyautogui.moveTo(origin[0], origin[1])
 
     count = 0
     for theta in np.arange(0, (num_rot * 2 + .01) * math.pi, increment):
-        # r = coefficients[0] + coefficients[1] * math.cos(coefficients[2] * theta)
-        r = theta
+        r = eval(equation)
         x = r * math.cos(theta)
         y = r * math.sin(theta)
 
@@ -26,7 +27,7 @@ def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, x
         x += origin[0]
         y += origin[1]
 
-        print((x, y))
+        # print((x, y))
 
         if count == 0:
             pyautogui.moveTo(x, y)
@@ -34,9 +35,10 @@ def draw(origin: tuple, coefficients: tuple, num_rot: float, increment: float, x
             pyautogui.dragTo(x, y, duration=.025)
         count += 1
 
+
 screen_size = pyautogui.size()
 
 print(screen_size)
 
-draw((screen_size.width / 2 + 100, screen_size.height / 2 + 75), (7, 0, 0), 6, math.pi / 192, (-35, 37.7), (-36.15, 33), (-300, 300), (-300, 300))
-
+draw((screen_size.width / 2 - 200, screen_size.height / 2), "theta * math.cos(theta)", 6, math.pi / 192, (0, 37.7), (-18.46, 17.6735),
+     (-350, 350), (-350, 350))
