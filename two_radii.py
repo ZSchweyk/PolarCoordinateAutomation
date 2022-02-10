01c2dd145aa14d2ff58447d682207be139edfd38
ZSchweyk <zeynschweyk@dpengineering.org>
2022-02-10 12:04:41 -0800

    for loop that takes into account 2 thetas, by adding pi to one theta

---
+++ two_radii.py
@@ -0,0 +1,16 @@
+from math import *
+import numpy as np
+
+equation = "sin(4 * theta)"
+
+for theta1 in np.arange(0, pi, pi/192):
+    theta2 = theta1 + pi
+
+    r1 = eval(equation.replace("theta", "theta1"))
+    r2 = eval(equation.replace("theta", "theta2"))
+
+    print("r1:", r1)
+    print("r2:", r2)
+
+print("Done drawing", equation)
+
